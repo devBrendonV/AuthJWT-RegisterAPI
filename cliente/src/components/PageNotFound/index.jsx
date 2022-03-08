ede8344bdd6c80c2ea1b38f06ece0c7e33efd6bd
Brendon Victor <devbrendonvs@gmail.com>
2022-03-07 23:01:03 -0300

    PageNotFound

--- cliente/src/components/PageNotFound/index.jsx
+++ cliente/src/components/PageNotFound/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Container } from '../LoggedScreen/styles'
+import { Container } from './styles'
+import { BiErrorAlt } from "react-icons/bi";
+
 const PageNotFound = () => {
   return (
-    <Container>PageNotFound</Container>
+      <Container>
+        <BiErrorAlt className='icon'></BiErrorAlt>
+        <h3>Error : 404</h3>
+        <h2>Page Not Found!!</h2>
+
+      </Container>
   )
 }
 
--- cliente/src/components/PageNotFound/styles.js
+++ cliente/src/components/PageNotFound/styles.js
@@ -1,4 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-`
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  background-color: #fafafa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 500px;
+  border-radius: 5px;
+  text-align: center;
+  color: #474747;
+  .icon {
+    font-size: 90px;
+    color: #f20707;
+  }
+`;
+
